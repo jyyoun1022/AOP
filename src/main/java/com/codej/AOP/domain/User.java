4fa5f8adb483bc759822aa0c527a0becfd597749
younjaeyeal <jyyoun1021@gmail.com>
2022-06-23 16:19:10 +0900

    Initial commit

---
+++ src/main/java/com/codej/AOP/domain/User.java
@@ -0,0 +1,4 @@
+package com.codej.AOP.domain;
+
+public class User {
+}
---
+++ src/test/java/com/codej/AOP/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codej.AOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
