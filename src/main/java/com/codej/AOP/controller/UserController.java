c2be5d6dd062cc2b538513d3b48f667a924732bf
younjaeyeal <jyyoun1021@gmail.com>
2022-06-24 14:11:38 +0900

    @Valid 속성

--- src/main/java/com/codej/AOP/controller/UserController.java
+++ src/main/java/com/codej/AOP/controller/UserController.java
@@ -10,9 +10,15 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -37,7 +43,16 @@ public class UserController {
 
     @PostMapping("/user")
     //x-www-form-urlencoded = (request.getParameter())
-    public ResponseEntity<String> save(@RequestBody JoinReqDTO dto){
+    public ResponseEntity<?> save(@Valid @RequestBody JoinReqDTO dto,BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            Map<String,String> errorMap = new HashMap<>();
+
+            for(FieldError error : bindingResult.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap,HttpStatus.BAD_REQUEST);
+        }
         log.info("save");
         System.out.println("dto = " + dto);
         userRepository.save(dto);
--- src/main/java/com/codej/AOP/domain/JoinReqDTO.java
+++ src/main/java/com/codej/AOP/domain/JoinReqDTO.java
@@ -2,10 +2,21 @@ package com.codej.AOP.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class JoinReqDTO {
+
+    @NotNull(message = "username 키값이 없습니다.")
+    @NotBlank(message = "username을 입력하세요")
+    @Size(max = 20,message = "username의 길이를 초과하였습니다.")
     private String username;
+
+    @NotNull(message = "비밀번호가 없습니다.")
     private String password;
+    @NotNull(message = "휴대번호 입력값은 필수 값입니다.")
     private String phone;
 
 }
